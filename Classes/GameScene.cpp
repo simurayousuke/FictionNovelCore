12e2cbdf89b3af1ebf5bb79d9dd5c2d7970b0c33
志庄 杨 <yzz0427@outlook.com>
2017-04-09 23:35:08 +0800

    add opacity

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -22,7 +22,11 @@ cocos2d::Scene* GameScene::createScene(Stage* stg)
 	scene->addChild(layer);
 	return scene;
 }
-
+/*
+	0.±³¾°Í¼
+	1.¿òÍ¼
+	2.ÎÄ×Ö
+*/
 bool GameScene::init()
 {
 	if (!Layer::init())
@@ -31,8 +35,8 @@ bool GameScene::init()
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 
-	auto closeItem = MenuItemImage::create("assets/CloseNormal.png","assets/CloseSelected.png",CC_CALLBACK_1(GameScene::menuCloseCallback, this));
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,origin.y + closeItem->getContentSize().height / 2));
+	auto closeItem = MenuItemImage::create("assets/CloseNormal.png", "assets/CloseSelected.png", CC_CALLBACK_1(GameScene::menuCloseCallback, this));
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2, origin.y + closeItem->getContentSize().height / 2));
 	auto menu = Menu::create(closeItem, NULL);
 	menu->setPosition(Vec2::ZERO);
 	this->addChild(menu, 1);
@@ -42,9 +46,9 @@ bool GameScene::init()
 		stage = StatusManager::getInstance()->getCurrentStage();
 
 	auto label = Label::createWithTTF(stage->getContext(), "fonts/" + stage->getFont(), stage->getFontSize());
-	label->setPosition(Vec2(origin.x +100,origin.y + Config::conversationHeight));
+	label->setPosition(Vec2(origin.x + label->getContentSize().width / 2 + 50, origin.y + Config::conversationHeight));
 	//label->setDimensions(visibleSize.width-50, Config::conversationHeight);
-	this->addChild(label, 3);
+	this->addChild(label, 2);
 
 	auto bg = Sprite::create("assets/" + stage->getBg());
 	bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
@@ -62,28 +66,31 @@ bool GameScene::init()
 
 	}
 
-	if (stage->getConversationBg() != "")
-	{
+	auto conversationBgPath = stage->getConversationBg();
+	if (conversationBgPath == "")
+		conversationBgPath = "conversationBg.png";
+	auto conversationBg = Sprite::create("assets/" + conversationBgPath);
+	conversationBg->setPosition(Vec2(visibleSize.width / 2 + origin.x, origin.y + Config::conversationHeight * 5 / 8));
+	conversationBg->setAnchorPoint(Point(0.5, 0.5));
+	conversationBg->setScale((visibleSize.width - 20) / conversationBg->getContentSize().width, Config::conversationHeight / conversationBg->getContentSize().height);
+	conversationBg->setOpacity(200);
+	this->addChild(conversationBg, 1);
 
-	}
-	
 	if (stage->hasButton())
 	{
 		int buttonNum = stage->getButtonNum();
 		Button* buttons = stage->getButtons();
-		
-		
 		for (int i = 0; i < buttonNum; ++i)
 		{
 			//½¨Á¢button
-			auto buttonItem = MenuItemFont::create(buttons[i].getContext(), CC_CALLBACK_1(GameScene::buttonClickCallback, this,buttons[i].getNextId()));
+			auto buttonItem = MenuItemFont::create(buttons[i].getContext(), CC_CALLBACK_1(GameScene::buttonClickCallback, this, buttons[i].getNextId()));
 			buttonItem->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height - (visibleSize.height - Config::conversationHeight - visibleSize.height*0.2) / buttonNum*(i + 1)));
 			auto buttonMenu = Menu::create(buttonItem, NULL);
 			buttonMenu->setPosition(Vec2::ZERO);
 			this->addChild(buttonMenu, 1);
 		}
-		
-	}	
+	}
+
 	stage = NULL;
 	return true;
 }
@@ -94,7 +101,7 @@ void GameScene::menuCloseCallback(Ref* pSender)
 	GameCore::getInstance()->nextStage();
 }
 
-void GameScene::buttonClickCallback(Ref* pSender,int id)
+void GameScene::buttonClickCallback(Ref* pSender, int id)
 {
 	GameCore::getInstance()->nextStage(id);
 }
